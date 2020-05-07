ec675740a2fb8068fd9b98da879f61fdef718336
Tim Rohaly <tr@202830.no-reply.drupal.org>
2020-05-07 07:50:38 +0000

    Issue #3133101 by TR: [D8] Coding standards - useless method overriding.

--- src/Form/FivestarForm.php
+++ src/Form/FivestarForm.php
@@ -110,13 +110,6 @@ class FivestarForm extends FormBase {
   /**
    * {@inheritdoc}
    */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    parent::validateForm($form, $form_state);
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $context = $form_state->get('context');
 
